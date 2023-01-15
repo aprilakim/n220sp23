580053b62bfa5c280b9ff6c847daa15e056b6efe
saffrondragon <107778664+saffrondragon@users.noreply.github.com>
2023-01-15 18:06:16 -0500

    rectangle triangles

--- labs/bookPractice/js/practice.js
+++ labs/bookPractice/js/practice.js
@@ -3,8 +3,9 @@ function setup() {
 }
 function draw() {
     background(204);
-    point(10, 60);
-    point(10, 10);
-    point(400, 100);
+    quad (158, 55, 199, 14, 392, 66, 351, 107);
+    triangle(347, 54, 392, 9, 392, 66);
+    triangle(158, 55, 290, 91, 290, 112);
+   
 
 }
