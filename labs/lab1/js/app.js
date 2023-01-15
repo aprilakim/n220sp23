9ccf7e3ad72935db78c5aa72784fede290a825a3
saffrondragon <107778664+saffrondragon@users.noreply.github.com>
2023-01-15 15:47:52 -0500

    Add example graphics

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -1,3 +1,15 @@
 function setup() {
     createCanvas(800,600);
+
+    background(107,130,45);
+   
+    circle(300,300,110);
+   
+    stroke(217,135,237);
+    strokeWeight(8);
+    fill(0,100,123);
+    rect(400,200,30,30);
+
+    stroke(2);
+    line(10,10,150,400);
 }
