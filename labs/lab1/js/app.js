d86d9fea964850346fb8b637796c0742de85642c
saffrondragon <107778664+saffrondragon@users.noreply.github.com>
2023-01-15 16:29:48 -0500

    add interactive circles

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -1,15 +1,23 @@
 function setup() {
     createCanvas(800,600);
 
-    background(107,130,45);
+    background(107,00,45);
    
     circle(300,300,110);
    
     stroke(217,135,237);
     strokeWeight(8);
-    fill(0,100,123);
+    fill(100,100,123);
     rect(400,200,30,30);
 
-    stroke(2);
+    stroke(1);
     line(10,10,150,400);
+}
+function draw() {
+    if (mouseIsPressed) {
+        fill(0);
+    } else {
+        fill(255);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
 }
