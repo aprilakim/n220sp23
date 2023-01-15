da0a1262b10cbbc1bd24d81085efb837081f78d0
saffrondragon <107778664+saffrondragon@users.noreply.github.com>
2023-01-15 16:43:09 -0500

    experiments

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -10,8 +10,11 @@ function setup() {
     fill(100,100,123);
     rect(400,200,30,30);
 
-    stroke(1);
-    line(10,10,150,400);
+    stroke(100,123,033);
+    strokeWeight(5);
+    fill(20,20,20);
+    line(100,50,200,1070);
+    
 }
 function draw() {
     if (mouseIsPressed) {
@@ -19,5 +22,5 @@ function draw() {
     } else {
         fill(255);
     }
-    ellipse(mouseX, mouseY, 80, 80);
+    ellipse(mouseX, mouseY, 8, 8);
 }
