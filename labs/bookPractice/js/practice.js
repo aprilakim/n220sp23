65786d22cd36381a399500790ab30325ca818736
saffrondragon <107778664+saffrondragon@users.noreply.github.com>
2023-01-16 20:09:01 -0500

    mod1 practice

--- labs/bookPractice/js/practice.js
+++ labs/bookPractice/js/practice.js
@@ -1,11 +1,11 @@
 function setup() {
-    createCanvas(480, 120);
+    createCanvas(880, 620);
 }
 function draw() {
     background(204);
-    quad (158, 55, 199, 14, 392, 66, 351, 107);
-    triangle(347, 54, 392, 9, 392, 66);
-    triangle(158, 55, 290, 91, 290, 112);
-   
-
-}
+    fill(144, 232, 231);
+    rect(50, 100, 300, 200, 12, 12);
+    fill(232, 144, 145);
+    ellipse(100, 150, 80, 80);
+    
+} //Pink ellipse on top of a blue beveled rectangle 
