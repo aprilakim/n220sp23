a47421a4caa5fb1114ff39e8b5665e89ad20d1af
saffrondragon <107778664+saffrondragon@users.noreply.github.com>
2023-01-23 15:57:40 -0500

    wrap around practice

--- labs/lab2/js/app.js
+++ labs/lab2/js/app.js
@@ -2,18 +2,35 @@
 22 January 2023
 Class 23523
 */ 
-let xPos = 0;
-let yPos = 10;
+//let xPos = 0;
+//let yPos = 10;
 
-function setup(){
-    createCanvas(400, 300);
+var radius = 40;
+var x = -radius;
+var speed = 0.5;
+
+function setup() {
+    createCanvas(240, 120);
+    ellipseMode(RADIUS);
 }
 
+function draw() {
+    background(0);
+    x += speed;         //increase the value of x
+    if (x > width+radius){
+        x = -radius;
+    }
+    arc(x, 60, radius, radius, 0.52, 5.76);
+}
+
+/*
 function draw(){
+    var fr = frameRate(30);
+    print(fr);
     background(0);
     xPos++; 
     yPos++;             //animate the circle
     fill(255, 102, 179);           
     circle(xPos, yPos, 30);
 }
-
+*/
