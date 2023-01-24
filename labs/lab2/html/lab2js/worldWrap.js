184bf41c3e8f86b46a2b326a125cd87b43c44314
saffrondragon <107778664+saffrondragon@users.noreply.github.com>
2023-01-23 19:19:46 -0500

    add world wrap algorithm and files

---
+++ labs/lab2/html/lab2js/worldWrap.js
@@ -0,0 +1,14 @@
+/*April Kim
+23 January 2023
+Class 23523
+*/
+
+/**Algorithm: 
+ * Use frame Count function with parameter set at 5 pixels per frame. 
+ * If x is greater than the width of the screen plus the radius of ball, 
+ * move ball to left edge with -radius. 
+ */
+
+function setup() {
+    createCanvas(800, 600);     //keep set at 800*600
+}
