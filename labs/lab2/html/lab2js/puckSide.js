3823f4869fcf960cf3cd3804b4b8be4d5d6014df
saffrondragon <107778664+saffrondragon@users.noreply.github.com>
2023-01-25 18:44:44 -0500

    puckSide assignment accomplished!

--- labs/lab2/html/lab2js/puckSide.js
+++ labs/lab2/html/lab2js/puckSide.js
@@ -3,12 +3,32 @@
 Class 23523
 */
 
-/* Algorithm: Constrain ball to canvas. Check the location of the mouse. 
+/* Algorithm: Check the location of the mouse. 
 If the position is less than 200, fill the circle blue.
 Or else, make red. 
 */
 
+radius = 40;
+var half;
+                                     
+
 function setup() {
-    createCanvas(400, 300);         //keep Canvas set at 400*300
-    background(104);
+    createCanvas(400, 300);                 //keep Canvas set at 400*300   
+    half = width/2;                         //store half the width of the canvas as variable
 }
+
+function draw() {
+    background(0);                          //background here allows only the most recent frame to appear
+    ellipse(mouseX, mouseY, radius, radius);//Draw a circle where the mouse is located
+    
+    if ( mouseX < half) {                   //if ball is on left half of canvas, circle is blue
+        fill(102, 179, 255);
+    } else{                                 //if ball is on right half of screen, circle is red
+        fill(255, 77, 77);
+    }
+}
+
+
+
+
+
