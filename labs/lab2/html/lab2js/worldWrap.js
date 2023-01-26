56ef457d1ad10097d3a9652258640895b96a817a
saffrondragon <107778664+saffrondragon@users.noreply.github.com>
2023-01-25 20:03:24 -0500

    blank slate for worldWrap

--- labs/lab2/html/lab2js/worldWrap.js
+++ labs/lab2/html/lab2js/worldWrap.js
@@ -9,22 +9,14 @@ Class 23523
  * move ball to left edge with -radius. 
  */
 
-var radius = 60;                 //set radius of ball
-var x = -radius;
-var fr = 5;
+
 
 function setup() {
     createCanvas(800, 600);     //keep set at 800*600
-    frameRate(fr);              //set the frame rate to 5 px per sec
-    ellipseMode(RADIUS);        //calculates radius as needed?
+
 }
 
 function draw () {
     
-    background(0);
-    x += fr;                    //move the ball by an increment of 5 or fr value
-    if (x > width + radius) {
-        x = -radius;
-    }
-    ellipse(20, 200, radius, radius);
+   
 }
