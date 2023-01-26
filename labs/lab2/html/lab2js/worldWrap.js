e111453dd9fb4656b6913de72299bf21658c3bf4
saffrondragon <107778664+saffrondragon@users.noreply.github.com>
2023-01-25 21:14:57 -0500

    made it footbally.. is that ok?

--- labs/lab2/html/lab2js/worldWrap.js
+++ labs/lab2/html/lab2js/worldWrap.js
@@ -9,19 +9,27 @@ var x = -radius;
 var speed = 2;
 
 function setup() {
-    createCanvas(800, 600);               //keep set at 800*600
+    createCanvas(800, 600);                 //keep set at 800*600
     ellipseMode(RADIUS);       
-    noStroke(); 
-
+    
 }
 
 function draw () {
-    background(0, 51, 17);
-   // background(0, 26, 51);               //set background here to refresh frame continuously
-    x += speed;                          //increase speed
-    if (x > width+radius) {              //if poistion of ball exceeds screen width and width of ball then reset position
+    background(0, 51, 17);                  //set background here to refresh frame continuously
+
+    for (var i = 0; i < 800; i += 100) {    //lines for "football field"
+        line(i, 0, i, 600);
+        stroke(255);
+    }
+    
+
+    x += speed;                             //increase speed
+    if (x > width+radius+40) {              //if poistion of ball exceeds screen width and width +40 for oval shape of ball then reset position
         x =-radius;
     }
-    ellipse(x, 300, radius, radius);     //ball
-    fill(255, 173, 51);                 //light blue
+    noStroke();
+    ellipse(x, 300, radius+40, radius);     //ball + 40 to elongate into football shape
+    fill(255, 173, 51);                     //light blue
+
+    
 }
