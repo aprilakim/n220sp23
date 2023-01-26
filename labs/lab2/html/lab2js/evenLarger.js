45562485d527403398ef74c8e0e772f606e5b788
saffrondragon <107778664+saffrondragon@users.noreply.github.com>
2023-01-25 19:28:00 -0500

    evenLarger mission accomplished!

--- labs/lab2/html/lab2js/evenLarger.js
+++ labs/lab2/html/lab2js/evenLarger.js
@@ -9,6 +9,25 @@ Class 23523
  * 
  */
 
+var radius = 1;
+
 function setup() {
-    createCanvas(400, 300); //able to choose any size I need
+    createCanvas(600, 400); 
+    noStroke();
+}
+
+function draw() {
+    background(102, 153, 255);
+    var fr = frameRate();
+    print(fr);
+    ellipse(width/2, height/2, radius, radius); //ensure ball is in the center
+    fill(255, 255, 153);
+    
+    if (fr ++) {                               //increase radius each time frame rate is incremented
+        radius ++;
+    }
+
+    if (radius > 200) {                         // if radius reaches 200, reset to 1 
+        radius = 1; 
+    }
 }
