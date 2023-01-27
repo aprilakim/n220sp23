5e9ce74db34b95eaa488215f11f395673458070d
saffrondragon <107778664+saffrondragon@users.noreply.github.com>
2023-01-26 19:27:55 -0500

    calculate speed on world wrap

--- labs/lab2/html/lab2js/worldWrap.js
+++ labs/lab2/html/lab2js/worldWrap.js
@@ -6,7 +6,10 @@ Class 23523
 
 var radius = 80;
 var x = -radius;
-var speed = 2;
+var speed = 1*1/12;                         //I was trying to figure out how to make the speed 5 pixels per second
+                                            //if there are 60 frames per second and 5 goes into 60 12 times..
+                                            //I multiplied 1 (1 = 100% speed= 60fps) * 1/12. I am probably overthinking. 
+//var speed = 1;                            //in case you want to speed it up              
 
 function setup() {
     createCanvas(800, 600);                 //keep set at 800*600
@@ -16,13 +19,12 @@ function setup() {
 
 function draw () {
     background(0, 51, 17);                  //set background here to refresh frame continuously/ green
-
     for (var i = 0; i < 800; i += 100) {    // vertical lines for "football field"
         line(i, 0, i, 600);
         stroke(255);
     }
     
-
+   
     x += speed;                             //increase speed
     if (x > width+radius+40) {              //if poistion of ball exceeds screen width and width +40 for oval shape of ball then reset position
         x =-radius;                         //reset position to just before canvas
