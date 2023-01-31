d9b124e7574a8aa1db5b1fb4a3aca26f7d242ac2
saffrondragon <107778664+saffrondragon@users.noreply.github.com>
2023-01-30 20:48:23 -0500

    drawingDrawer algorithm checkin

--- labs/lab3/js/drawingDrawer.js
+++ labs/lab3/js/drawingDrawer.js
@@ -4,3 +4,7 @@ April Kim
 Class 23523
 */
 
+//Create design/shape and block it into a (draw) function
+//Set variables for x and y so that they can be incremented/ move around (in a for loop?)
+//Set variables for mouseX & mouseY similar to above step so mouse movement can be tracked and updated
+//Call draw function 3 different times with different x & y values so that 3 images appear simultaneously
