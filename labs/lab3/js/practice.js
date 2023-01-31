eaf08812106f906ca0d57ca26b84dccd9f9c1952
saffrondragon <107778664+saffrondragon@users.noreply.github.com>
2023-01-30 22:10:43 -0500

    Update practice.js

--- labs/lab3/js/practice.js
+++ labs/lab3/js/practice.js
@@ -1,26 +1,16 @@
-var num = 60;
-var x = [];
-var y = [];
 
- function setup() {
-    createCanvas(800, 600);
-    noStroke();
+var tabby, siamese, lucky;
+var friendly, mean, apathetic;
 
-    for (var i = 0; i < num; i++) {
-        x[i] = 0;
-        y[i] = 0;
-    }
- }
-  function draw() {
-    background(0);
-    for (var i = num-1; i>0; i--){
-        x[i] = x[i-1];
-        y[i] = y[i-1];
-    }
-    x[0] = mouseX;
-    y[0] = mouseY;
-    for (var i = 0; i < num; i++){
-        fill(i*4);
-        ellipse(x[i], y[i], 40, 40);
-    }
+function setup() {
+    tabby = new Cat("Tiger the Tabby", "bites");
+    siamese = new Cat("Sammy the Siamese", "wants cuddles");
+    lucky = new Cat("Lucky the lucky cat", "doesn't care");
 }
+    //The following is an example of an object with variables that update the values 
+    //accordingly using the "this" keyword
+
+    function Cat(catType, catPersonality) {
+        this.name = catType;
+        this.name = catPersonality;
+    }
