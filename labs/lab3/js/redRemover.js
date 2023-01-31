b82ae99d84977af50bd139ae4acf7973023d449c
saffrondragon <107778664+saffrondragon@users.noreply.github.com>
2023-01-30 21:04:53 -0500

    redRemover algorithm checkin

--- labs/lab3/js/redRemover.js
+++ labs/lab3/js/redRemover.js
@@ -3,3 +3,7 @@ April Kim
 29 January 2023
 Class 23523
 */
+
+//Create a variable for the red part of (r,g,b) 
+//Use for loop to increment the value of red variable, starting at red and fading to not red In a function
+//Create an instance that tests the function with drawing a circle
