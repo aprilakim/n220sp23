ff4c9e7058e7baba69493c9ceb0269a0fa84a851
saffrondragon <107778664+saffrondragon@users.noreply.github.com>
2023-01-30 19:11:50 -0500

    practice arrays

--- labs/lab3/js/practice.js
+++ labs/lab3/js/practice.js
@@ -1,12 +1,19 @@
-   function setup() {
-    print("Ready to roll");
-    rollDice(6);
-    rollDice(6);
-    rollDice(6);
-    print("Finished!");
-   }
+var x = [];
 
-   function rollDice(numSides) {
-    var d = 1 + int(random(numSides));
-    print("Rolling... " + d);
+ function setup() {
+    createCanvas(240, 120);
+    noStroke();
+    fill(255, 200);
+    for (var i =0; i < 3000; i++) {
+        x[i] = random(-1000, 200);
+    }
+ }
+   function draw() {
+    background(0);
+    for (var i = 0; i < x.length; i++) {
+        x[i] +=0.5;
+        var y = i * 0.4;
+        arc(x[i], y, 12, 12, 0.52, 5.76);
+    }
    }
+  
