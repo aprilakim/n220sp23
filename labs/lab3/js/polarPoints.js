fd69066235e0c2a5b62702cf6c89f8c4b181d4ff
saffrondragon <107778664+saffrondragon@users.noreply.github.com>
2023-01-30 20:59:22 -0500

    polarPoint algorithm checkin

--- labs/lab3/js/polarPoints.js
+++ labs/lab3/js/polarPoints.js
@@ -3,3 +3,13 @@ April Kim
 29 January 2023
 Class 23523
 */
+
+//polarPoint function contains a variable "x" that has the formula r * Math.sin(mouseX)
+//polarPoint function contains a variable "y" that has the formula r * Math.cos(mouseY)
+//return createVector(x,y)
+
+
+//create object instance
+//Set a variable that is equal to the same value as return createVector(x.y)
+//Set a new point of origin with translate function
+//draw circle in the object 
