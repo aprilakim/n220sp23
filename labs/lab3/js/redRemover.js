403c69523be6521f848bbb70005c49db8615b848
April Kim <107778664+saffrondragon@users.noreply.github.com>
2023-02-01 22:49:31 -0500

    not done but tired

--- labs/lab3/js/redRemover.js
+++ labs/lab3/js/redRemover.js
@@ -8,3 +8,30 @@ Class 23523
 //Use for loop to increment the value of red variable, starting at red and fading to not red In a function
 //Create an instance that tests the function with drawing a circle
 
+//let x = 0;
+
+//let noRed = removeRed(color(170, 200, 150));
+//fill(noRed);
+//x = x + 1;
+//red = 255
+//color = 255;
+
+
+function setup() {
+    createCanvas(800, 600);
+
+}
+
+function draw() {
+    background(217, 179, 255); 
+    ellipse(mouseX, mouseY, 80, 80);
+    fill(255,0,0);
+   // removeRed();
+}
+/*
+function removeRed(color){
+    let noRed = removeRed(color(170, 200, 150));
+    fill(noRed);
+    red.setRed(0)
+}
+*/
