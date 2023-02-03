0387a6b8c886729b35137f3d1d358acd60534aa5
April Kim <107778664+saffrondragon@users.noreply.github.com>
2023-02-02 20:20:03 -0500

    Update polarPoints.js

--- labs/lab3/js/polarPoints.js
+++ labs/lab3/js/polarPoints.js
@@ -11,5 +11,24 @@ Class 23523
 
 //create object instance
 //Set a variable that is equal to the same value as return createVector(x.y)
-//Set a new point of origin with translate function
+
 //draw circle in the object 
+
+function setup() {
+    createCanvas(800, 600);
+}
+function draw() {
+    background(204, 255, 153);
+    translate(100, 100);                //Set a new point of origin with translate function
+   // let res = polarPoint(createVector());
+    ellipse(res.x, res.y, 10, 10);
+    polarPoint();
+    
+;}
+
+function polarPoint(r) {                //create Cartesian grid 
+    let x = r * Math.sin(mouseX);           
+    let y = r * Math.cos(mouseX);
+    return createVector(x, y);
+    
+}
