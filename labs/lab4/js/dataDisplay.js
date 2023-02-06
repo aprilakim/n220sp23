7f2563487d12a507409aedc766132b79dea9ee3d
April Kim <107778664+saffrondragon@users.noreply.github.com>
2023-02-06 15:10:39 -0500

    dataDisplay algorithm checkin

---
+++ labs/lab4/js/dataDisplay.js
@@ -0,0 +1,11 @@
+/*
+April Kim
+5 February 2023
+N220 sp23
+Class #23523
+*/
+
+/*-Create object with properties width, height, color, and position of ___ --*/
+/*- draw object by refering to width, heigt, etc properties in object*
+/*like a protoype or template?/
+/*make a function that take an argument and draws the object accordingly*/
