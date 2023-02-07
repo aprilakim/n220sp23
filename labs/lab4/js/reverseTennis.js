c5147c991b02a6bd33f24cbe896fd4d093e290cc
April Kim <107778664+saffrondragon@users.noreply.github.com>
2023-02-06 19:05:20 -0500

    all 3 algorithm check ins now complete

--- labs/lab4/js/reverseTennis.js
+++ labs/lab4/js/reverseTennis.js
@@ -4,3 +4,9 @@ April Kim
 N220 sp23
 Class #23523
 */
+
+/*----------Object Tall Rectangle A --include properties (x,y,width,height) ----x pos on left half of screen---*/
+/*----------Object Tall Rectangle B --include properties (x,y,width,height)-----x pos on right half of screen----*/
+/*------if up key is pressed, Rect A goes up(iterate up the y axis) and Rect B goes down--(down the y axis-----------*/
+/*------else if down key is pressed, Rect A goes down(iterate down the y axis) and Rect B goes up--( the y axis-------------*/
+/*-------------------*/
--- labs/lab4/js/worldWrapRefactor.js
+++ labs/lab4/js/worldWrapRefactor.js
@@ -4,3 +4,10 @@ April Kim
 N220 sp23
 Class #23523
 */
+
+
+/*---World wrap ball gets turned into an object w properties size, color, velocity ----------------*/
+/*--!No global or hard coded variables... just object w parameters-----------------*/
+/*-------------------*/
+/*-------------------*/
+/*-------------------*/
