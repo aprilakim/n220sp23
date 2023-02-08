5a4bec4d8af831354ec1c6a19932be0916a8c44a
April Kim <107778664+saffrondragon@users.noreply.github.com>
2023-02-08 17:54:39 -0500

    dataDisplay primitive

---
+++ app.js
@@ -0,0 +1,7 @@
+app.js
+
+let sum = 0;
+
+for (i = 0 ; i< n ; ++1);
+    ++sum;
+    console.log(sum)
--- labs/lab4/js/dataDisplay.js
+++ labs/lab4/js/dataDisplay.js
@@ -9,3 +9,30 @@ Class #23523
 /*- draw object by refering to width, heigt, etc properties in object*
 /*like a protoype or template?/
 /*make a function that take an argument and draws the object accordingly*/
+
+
+let star = {
+    width: 100,
+    height: 100,
+    color: "#ffff66",
+    xPos: 350,
+    yPos: 250,
+};
+
+function setup() {
+    createCanvas(800, 600);
+    background(0);
+    
+
+}
+
+function draw() {
+    fill(star.color);
+    
+    rect(star.xPos, star.yPos, star.width, star.height);
+        if(mouseIsPressed){
+            fill(star.color = "#4d79ff");
+        }
+
+
+}
