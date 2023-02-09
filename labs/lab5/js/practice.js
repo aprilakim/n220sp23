8b6f5892451f740a58c8a6f9e4301e6dcba0f4d8
April Kim <107778664+saffrondragon@users.noreply.github.com>
2023-02-09 11:08:13 -0500

    lab 5 start

---
+++ labs/lab5/js/practice.js
@@ -0,0 +1,12 @@
+function checkResponse(time) {
+    console.log(time);
+    if (time == morning)
+        console.log("It works");
+}
+/*      
+time = {
+    morning: 0000-1100 ,
+    afternoon: 1101- ,
+    evening:
+}
+*/
