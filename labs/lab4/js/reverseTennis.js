061584f7a782d037e76b938d49151e8e4690e201
April Kim <107778664+saffrondragon@users.noreply.github.com>
2023-02-08 19:47:33 -0500

    Update reverseTennis.js

--- labs/lab4/js/reverseTennis.js
+++ labs/lab4/js/reverseTennis.js
@@ -10,3 +10,21 @@ Class #23523
 /*------if up key is pressed, Rect A goes up(iterate up the y axis) and Rect B goes down--(down the y axis-----------*/
 /*------else if down key is pressed, Rect A goes down(iterate down the y axis) and Rect B goes up--( the y axis-------------*/
 /*-------------------*/
+
+/*---Create tennis ball constructor----*/
+
+class TennisBall {
+    constructor(type) {
+        this.type = type;    //update the type accordingly
+        this.x = x;
+        this.y = y; 
+        this.width = 50;
+        this.height = 50;
+    }
+}
+
+/*-----Create function that moves the left ball towards the left and the right ball towards the right--*/
+
+TennisBall.prototype.move = function 
+
+let tennisBallL = new TennisBall ();
