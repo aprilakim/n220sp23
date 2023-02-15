b4cf6321e007f25bb48a3100b35eca08c881fac1
April <aprilakim07@gmail.com>
2023-02-15 16:13:09 -0500

    progress, but not done

--- labs/lab5/js/peakPixels.js
+++ labs/lab5/js/peakPixels.js
@@ -5,10 +5,25 @@ N220 sp23
 Class #23523
 */
 
+
 /*Algorithm:---------------------------------------------------------------------------------*/
-/*--Make an obect that stores height: 100px, width: 100px, and color:green(background)-------*/
-/*--Link the object id with the <div> element-------*/
+/*--Make an obect that stores Varaibale for height: 100px, Variable for width: 100px, and color:green(background)-
+    located in peakPixels.css------*/
+/*--Link the <div> element with id in HTML document-------*/
+
+
 /*--If mouse is clicked, increase div by 1.1* the original size-------*/
+
+let shape = document.getElementById("shapeB");
+let h = 100;
+
+/*--This function is not invoked anywhere---*/
+
+function growSquare() {
+    if (mouseClicked){
+        shape.style.backgroundColor = "#000000"; 
+    }
+}
 
 /*--------------------------------------------------------------------------------------------*/
 /*--Prof Instructions:Write the markup and JS to place a square div on the page. 100px by 100px, green background.
