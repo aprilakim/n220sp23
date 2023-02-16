6b47d210bc24ca0a3742c53a5bb4e5437cd36501
April <aprilakim07@gmail.com>
2023-02-16 15:56:37 -0500

    working code

--- labs/lab5/js/peakPixels.js
+++ labs/lab5/js/peakPixels.js
@@ -6,36 +6,29 @@ Class #23523
 */
 
 
-/*Algorithm:---------------------------------------------------------------------------------*/
-/*--Make an obect that stores Varaibale for height: 100px, Variable for width: 100px, and color:green(background)-
-    located in peakPixels.css------*/
+/*Algorithm:----------------------------------------------------------------*/
+
 /*--Link the <div> element with id in HTML document-------*/
 
 
 let shape = document.getElementById("shapeB");
-/*
+shape.style.width= "100px";
+shape.style.height= "100px";
+
 let h = 100;
 let w = 100;
-*/
+
 /*--If mouse is clicked, increase div by 1.1* the original size-------*/
 /*--Notes: the square grows width and height ONE time only. How can I 
 (store the variables so that the square can keep increasing in size?)---*/
 
-    
     function growSquare() {
+        
+        shape.style.width = `${w * 1.1}px`; 
+        shape.style.height = `${h * 1.1}px`;  
+        h *= 1.1 ;
+        w *= 1.1 ;
+        
+}       
 
-    shape.style.width = `${h * 1.1}px`; 
-    shape.style.height = var(--w) * 1.1 ;
-         
-    
-}
 
-/*--------------------------------------------------------------------------------------------*/
-/*--Prof Instructions:Write the markup and JS to place a square div on the page. 100px by 100px, green background.
-Make it so that when the div its clicked, it increases its size by 10% every time.
-
-Hints:
-
-Make a variable to store the height and with of the object
-to set the height and width, set to varName + "px"
-10% is .1 bigger,or 1.1 * the original size-------*/
