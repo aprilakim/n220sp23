fec2846c385dffca5092d55075320f3d5772f84d
April <aprilakim07@gmail.com>
2023-02-19 16:14:30 -0500

    loopButBig algorithm complete

---
+++ labs/lab6/js/loopButBig.js
@@ -0,0 +1,16 @@
+/*
+April Kim
+19 February 2023
+N220 sp23
+Class #23523
+*/
+
+/*--Algorithm:---------------*/
+
+/* Create <div> with id ="counter" 
+
+Initialization begins at 1000, if it's less than 5000, add 1000 to the iterated variable  
+
+display the value in the <div>
+
+/*---------------------------*/
