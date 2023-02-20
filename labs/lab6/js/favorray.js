cd5f4c469cdfecaa46b9b1deb33b2c3c4e5fb44b
April <aprilakim07@gmail.com>
2023-02-20 14:53:05 -0500

    favorray algorithm checkin

---
+++ labs/lab6/js/favorray.js
@@ -0,0 +1,17 @@
+/*
+April Kim
+19 February 2023
+N220 sp23
+Class #23523
+*/
+
+/*--Algorithm:
+ Create an array of strings with 5+ of my favorite things
+
+ create a loop that iterates through and 
+  
+ concatencate the result with +".., is one of my favorite things"
+
+
+
+----------------------*/
