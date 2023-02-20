ee66634a6ab78e4845387efcdacb3b63610710fb
April <aprilakim07@gmail.com>
2023-02-20 14:57:06 -0500

    runnerUp algorithm check in

---
+++ labs/lab6/js/runnerUp.js
@@ -0,0 +1,20 @@
+/*
+April Kim
+19 February 2023
+N220 sp23
+Class #23523
+*/
+
+/*--Algorithm:
+
+    Compute 3 random numbers ? to simulate race times
+
+    Create an array that takes the numbers from lowest to highest 
+        ?
+
+    Display the Runner ups on the screen #2 in a div="second" and #3 in a div="third"
+
+
+
+
+-----------------*/
