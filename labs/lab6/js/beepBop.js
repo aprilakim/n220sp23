3cd22c32769f2a6e51f03272d1e355e06fa2b2cb
April <aprilakim07@gmail.com>
2023-02-20 14:46:30 -0500

    beepBop Algorithm check in

---
+++ labs/lab6/js/beepBop.js
@@ -0,0 +1,29 @@
+/*
+April Kim
+19 February 2023
+N220 sp23
+Class #23523
+*/
+
+/*--Algorithm:
+
+Do I need array [0-24]
+
+Create div linked with id="beeper"
+ For loop- if i is less than 25 run code
+
+    if i/3 with no modulas
+    output word "beep"
+
+    if i/5 with no modulas
+    ouput word "bop"
+
+    if i/3 && i/5 
+    output "beep" + "bop"
+
+Expected results:
+0-24 numbers will be ran.. Numbers divisible by 3: (3, 6, 9, 12, 15, 18, 21, 24)-8 digits
+                           Numbers divisible by 5: (5, 10, 15, 20) - 4 digits
+                           Numbers divisble by 3 && 5: (15)- 1 digit
+    >> beep(3) bop(5) beep(6) beep(9) bop(10) beep(12) beepbop(15) beep(18) bop(20) beep(21) beep(24) 
+---------------*/
