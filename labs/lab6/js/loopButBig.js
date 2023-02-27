dc9a902ebad7ca96fcd5a402662c2d18a9dc2fbf
April <aprilakim07@gmail.com>
2023-02-27 15:26:37 -0500

    loopButBig works

--- labs/lab6/js/loopButBig.js
+++ labs/lab6/js/loopButBig.js
@@ -8,28 +8,23 @@ Class #23523
 /*--Algorithm:---------------*/
 
 /* Create <div> with id ="counter" */
-let counter = document.getElementById("counter");
-counter.style.width= "450px";
-counter.style.height= "200px";
-counter.style.backgroundColor= "#4287f5";
+let count = document.getElementById("counter");
+count.style.width= "450px";
+count.style.height= "300px";
+count.style.backgroundColor= "#4287f5";
+count.style.textAlign= "center";
+count.style.padding= "10px";
+count.style.fontSize = "25px";
 
-let num = 1000;
 
-let newNums = document.getElementById("nums");
-newNums.style.textA
-newNums.style.height="150px";
-
-/*--when mouse does something, change the style---------*/
-function displayNums() {
-        for(i = 1000; i < 5000; ++1000);
-        newNums.innerHTML = num[i];
-        console.log(num);
-    }
-    
-    /*---------*/
-/*
+/*--------------------------------------------------------------------------------------
+when mouse does something, change the style-------
 Initialization begins at 1000, if it's less than 5000, add 1000 to the iterated variable  
+display the value in the <div>---------------------------------------------------------*/
 
-display the value in the <div>
-
-/*---------------------------*/
+function displayNums(){
+    for (let number = 1000; number <= 5000; number+= 1000){
+        count.innerHTML += number + "<br>";
+    }
+}
+  
