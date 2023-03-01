ad3161dce3fe66f78879a39a1b822c1ce345819c
April <aprilakim07@gmail.com>
2023-03-01 16:06:42 -0500

    ugh

--- labs/midtermReview/js/highlight.js
+++ labs/midtermReview/js/highlight.js
@@ -12,9 +12,13 @@ remove the bold/background highlight from any other divs
 add the clicked div's text into a seperate div at the top of the screen
 -------------------------------------------------*/
 
+//OMG do i have to make a function for each div clicked.. long hand
+//then  maybe I can figure out DRY ???
+
 //Algorithm: Use onclick in each of the 6 divs
 //set global properties that get updated in a function 
 //function should make text bold and change background color
+//separate function or if statement to clear others? 
 //as well as reset any highlighted divs back to the original
 //have a separate div that displays the text of the highlighted div
 
@@ -27,20 +31,52 @@ display.style.width = "350px";
 display.style.padding = "15px";
 
 //class needed so that ONE function can be applied to ANY of the words that are clicked
-const selected = document.getElementsByClassName("animal");
+let selected = document.getElementsByClassName("animal");
 
 //class not working for me RN so doing id just to make s happen
 let animalOne = document.getElementById("penguin");
-//
-//Do I need an array to store the animals? 
+let animalTwo = document.getElementById("octopus");
+let animalThree = document.getElementById("bear");
+let animalFour = document.getElementById("seahorse");
+let animalFive = document.getElementById("parrot");
+let amimalSix = document.getElementById("sloth");
 
+//store animals in array (not sure if essential, yet)
+let animalArray = [animalOne, animalTwo, animalThree, animalFour, animalFive, animalSix];
+
+//nothing happened when I tried...
+//let all = document.body.getElementsByTagName("div");
+//all.style.padding = "15px";
 
 //Function: highlight and bold text,reset other divs 
 function highlightText(){   
-   animalOne.style.backgroundColor = "#74d9ed";
+   animalOne.style.backgroundColor = "#f5695f";
    animalOne.style.fontWeight = "bold";
    display.innerHTML = "Penguin";
 
-   selected.style.backgroundColor = "";
+   animalTwo.style.backgroundColor = "#f78f39";
+   animalTwo.style.fontWeight = "bold";
+   display.innerHTML = "Octopus";
+
+   animalThree.style.backgroundColor = "#f3f582";
+   animalThree.style.fontWeight = "bold";
+   display.innerHTML = "Bear";
+
+   animalFour.style.backgroundColor = "#82f593";
+   animalFour.style.fontWeight = "bold";
+   display.innerHTML = "Seahorse";
+
+   animalFive.style.backgroundColor = "#82e5f5";
+   animalFive.style.fontWeight = "bold";
+   display.innerHTML = "Parrot";
+
+   //why isnt sloth working????????
+   animalSix.style.backgroundColor = "#be89f0";
+   animalSix.style.fontWeight = "bold";
+   display.innerHTML = "Sloth";
    
+
+   //nothing happened when I tried...
+   //animalArray[].style.backgroundColor = "#74d9ed";
 }
+
