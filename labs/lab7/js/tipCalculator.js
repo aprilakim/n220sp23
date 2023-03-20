79c8afeee8ac44b1669cfd2c6b62f58c90055316
April <aprilakim07@gmail.com>
2023-03-20 16:47:47 -0400

    tip calculator algorithm check in

--- labs/lab7/js/tipCalculator.js
+++ labs/lab7/js/tipCalculator.js
@@ -6,3 +6,11 @@ Class #23523
 */
 
 /*--Algorithm:---------------*/
+
+/*--Assuming input element and button element are created in HTML file:--*/
+/*--Create function getBillAmount--*/
+/*--save user input in  variable that ensures input is numerical--*/
+/*--calculate tip by multiplying user input by 20%--*/
+/*--add tip amount to user input--*/
+/*--Output tip to screen using innnerHTML--*/
+/*--Output the tip + bill = total amount to screen using innnerHTML--*/
