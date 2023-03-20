e5d780ae514dbab39f940a5fef454243a8a6d5b0
April <aprilakim07@gmail.com>
2023-03-20 19:12:21 -0400

    tip calculator algorithm

--- labs/lab7/js/numberGuesser.js
+++ labs/lab7/js/numberGuesser.js
@@ -6,3 +6,8 @@ Class #23523
 */
 
 /*--Algorithm:---------------*/
+/*--store user input in variable in empty string?--*/
+/*--create function that compares user input to hard coded number--*/
+/*---if user number is less than set number, output says too low-*/
+/*---if user number is greater than set number, output says too high-*/
+/*--if user guesses correct number, say correct! the number is __ --*/
