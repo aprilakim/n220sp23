42a9bb441a95af8ea8ced4d38591727131caa187
April <aprilakim07@gmail.com>
2023-03-20 16:43:25 -0400

    greeter algorithm check in

--- labs/lab7/js/greeter.js
+++ labs/lab7/js/greeter.js
@@ -6,3 +6,7 @@ Class #23523
 */
 
 /*--Algorithm:---------------*/
+/*--Assuming input element and button element are created in HTML file:--*/
+/*--Create function getUsername--*/
+/*--save user input in variable--*/
+/*--Output username to screen using innnerHTML--*/
