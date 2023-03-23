b5b156632be55f6afbffd8991bbdfbd8d932cb13
April <aprilakim07@gmail.com>
2023-03-23 14:17:44 -0400

    greeter complete

--- labs/lab7/js/greeter.js
+++ labs/lab7/js/greeter.js
@@ -8,5 +8,13 @@ Class #23523
 /*--Algorithm:---------------*/
 /*--Assuming input element and button element are created in HTML file:--*/
 /*--Create function getUsername--*/
+
+let txtGreeting = document.getElementById("txtGreeting");
+let divOutput = document.getElementById("divOutput");
+
+function getName(){
+    divOutput.innerHTML = "Hello, " + txtGreeting.value + "!";
+    console.log("Hello, " + txtGreeting.value); 
+}
 /*--save user input in variable--*/
 /*--Output username to screen using innnerHTML--*/
