1cc7516720d0305dba18a3a4f3bb563b250e2824
April <aprilakim07@gmail.com>
2023-03-23 22:27:52 -0400

    looks okay but needs debugging

--- labs/lab7/js/numberGuesser.js
+++ labs/lab7/js/numberGuesser.js
@@ -5,9 +5,38 @@ N220 sp23
 Class #23523
 */
 
-/*--Algorithm:---------------*/
+/*--Algorithm:----*/
+/*Prof Instructions:
+Create a number guessing name, using an input and a button to gather a number.
+The number to be guessed should be a hard-coded whole number between 1 and 20.
+Tell the user if the number is too high, equal to, or too low than a number you have hard-coded in your application.
+Remove the text in the input when the user clicks the button.
+
+-----------*/
+let answer = 7;
+let txtGuess = document.getElementById("txtGuess");
+let dvOutput = document.getElementById("dvOutput");
+
 /*--store user input in variable in empty string?--*/
 /*--create function that compares user input to hard coded number--*/
-/*---if user number is less than set number, output says too low-*/
-/*---if user number is greater than set number, output says too high-*/
-/*--if user guesses correct number, say correct! the number is __ --*/
+function compareGuess(){
+    /*---if user number is greater than set number, output says too high-*/
+    if txtGuess.value > answer {
+        dvOutput.innerHTML = "Too high!";
+    }
+
+    /*---if user number is less than set number, output says too low-*/
+    if txtGuess.value < answer {
+        dvOutput.innerHTML = "Too low!";
+    }
+    
+    /*--if user guesses correct number, say correct! the number is __ --*/
+    if txtGuess.value = answer {
+        dvOutput.innerHTML = "You are correct! The answer is" + answer +"!"
+    }
+    else {
+        dvOutput.innerHTML = "I don't understand"
+    }
+}
+
+
