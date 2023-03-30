5b0734fb9ad3866120eb7d9289f2a575af15f03a
April <aprilakim07@gmail.com>
2023-03-29 23:41:41 -0400

    edit

--- labs/lab8/js/three.js
+++ labs/lab8/js/three.js
@@ -5,4 +5,13 @@ N220 sp23
 Class #23523
 */
 
-/*--Algorithm:---------------*/
+/*--Algorithm:----
+Make a function that takes a string, and removes every instance of 
+"#" from the string, and returns the "cleaned up" version without 
+the hashtags/octothorpes. 
+
+Write a web page to test the function - one button and input, and one 
+div. When the button is pressed, take the input, use the function to 
+remove the hashtags, and show the result string on the page.-----------*/
+
+//function 
