681bcf5af3c4c48a5c6e8bf7ff1924a32db3909d
April <aprilakim07@gmail.com>
2023-03-29 22:27:49 -0400

    random number algorithm check in

--- labs/lab8/js/one.js
+++ labs/lab8/js/one.js
@@ -8,7 +8,13 @@ Class #23523
 /*--Algorithm:---------------*/
 //--Accept user input, only if it is a number--//
 //--Or else ask for a number input--//
+function getInput(){
+
+}
 //--Function that determines if there is a remainder when number is divided by 7:--//
-//--If there is no remainder, return that the number is divisible by 7--//
+//--If there is no remainder, return TRUE that the number is divisible by 7--//
     //ie 7, 14, 21, 28, etc..//
-//--If there is a remainder, return that the number is not a factor of 7--//
+function divideNumber(){
+
+}
+//--If there is a remainder, return FALSE that the number is not a factor of 7--//
--- labs/lab8/js/two.js
+++ labs/lab8/js/two.js
@@ -5,4 +5,9 @@ N220 sp23
 Class #23523
 */
 
-/*--Algorithm:---------------*/
+/*--Algorithm:----
+
+  -----------*/
+
+//generateRandom function generates RANDOM number between 0-10 (*10)FLOOR
+//when button is clicked, display random number to screen
